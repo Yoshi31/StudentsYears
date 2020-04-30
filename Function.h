41bf1e51bc56c2f2cf3b13eb1618bf50f265f4f1
Yoshi31 <62884580+Yoshi31@users.noreply.github.com>
2020-04-30 16:25:19 +0300

    Update Function.h

--- Function.h
+++ Function.h
@@ -1,9 +1,9 @@
 #pragma once
-void addfilm();
-void deletefilm();
+void addfilm(string fileName);
+void deletefilm(string fileName);
 void searchfilm();
-void searchfilm_name();
-void searchfilm_genre();
-void listfilm();
-void changefilm();
+void searchfilm_name(string fileName);
+void searchfilm_genre(string fileName);
+void listfilm(string fileName);
+void changefilm(string fileName);
 int menu_select();
